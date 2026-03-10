38915d387426af463b589ed06724f11e08baaa34
Kauã Morinaga Prado <kauamorinaga413@gmail.com>
2026-03-09 22:44:52 -0300

    adicionando dump do banco de dados

---
+++ Dumpacademia.sql
@@ -0,0 +1,85 @@
+CREATE DATABASE  IF NOT EXISTS `ACADEMIA` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `ACADEMIA`;
+-- MySQL dump 10.13  Distrib 8.0.45, for Linux (x86_64)
+--
+-- Host: localhost    Database: ACADEMIA
+-- ------------------------------------------------------
+-- Server version	8.0.45-0ubuntu0.22.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ALUNOS`
+--
+
+DROP TABLE IF EXISTS `ALUNOS`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ALUNOS` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_planos` int NOT NULL,
+  `nome` varchar(30) NOT NULL,
+  `nascimento` date DEFAULT NULL,
+  `cpf` varchar(14) DEFAULT NULL,
+  `sexo` enum('M','F') DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_planos` (`id_planos`),
+  CONSTRAINT `ALUNOS_ibfk_1` FOREIGN KEY (`id_planos`) REFERENCES `PLANOS` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ALUNOS`
+--
+
+LOCK TABLES `ALUNOS` WRITE;
+/*!40000 ALTER TABLE `ALUNOS` DISABLE KEYS */;
+INSERT INTO `ALUNOS` VALUES (1,1,'Maria Souza','2000-05-10','123.456.789-00','F'),(2,2,'Kaua Morinaga','2006-08-28','123.444.789-52','M'),(3,3,'Joaozinho','1999-07-03','331.456.981-15','M');
+/*!40000 ALTER TABLE `ALUNOS` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `PLANOS`
+--
+
+DROP TABLE IF EXISTS `PLANOS`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `PLANOS` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome_plano` varchar(30) NOT NULL,
+  `valor` decimal(6,2) NOT NULL,
+  `duracao_meses` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `PLANOS`
+--
+
+LOCK TABLES `PLANOS` WRITE;
+/*!40000 ALTER TABLE `PLANOS` DISABLE KEYS */;
+INSERT INTO `PLANOS` VALUES (1,'Básico',79.90,1),(2,'Prata',110.90,3),(3,'Ouro',129.90,12);
+/*!40000 ALTER TABLE `PLANOS` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-03-04 23:06:07
